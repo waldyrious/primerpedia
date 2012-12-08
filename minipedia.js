94217ecece918c015c46ae93c244297bdffcc4ed
Waldir Pimenta <waldir@email.com>
2012-12-08 19:39:08 +0000

    remove nonstandard Object.firstChild (thanks MatmaRex)

--- minipedia.js
+++ minipedia.js
@@ -1,28 +1,20 @@
-// Add firstChild property to Javascript's Object prototype
-// https://gist.github.com/4240760
-// alternative: http://stackoverflow.com/a/2769741/266309
-Object.defineProperty( Object.prototype, 'firstChild', {
-	get: function () {
-		return this[Object.keys(this)[0]];
-	}
-});
-
 function onFormSubmit(){
 	apiUrl = "http://en.wikipedia.org/w/api.php?";
 	// API request to load non-random page:
 	// action=parse&page=Concise_Wikipedia&section=0&prop=text&format=txtfm&disablepp
 	$.ajax({
 		// first section of a random article. API query devised by http://stackoverflow.com/q/13517901/266309
-		url: apiUrl + 'action=query&prop=revisions&rvprop=content&rvparse=&rvsection=0&generator=random&grnnamespace=0&format=json',
+		url: apiUrl + 'action=query&prop=revisions&rvprop=content&rvparse=&rvsection=0&generator=random&grnnamespace=0&indexpageids=true&format=json',
 		data: {
 		    format: 'json'
 		},
 		dataType: 'jsonp',
 		success: function(jsonObject) {
-			var article = jsonObject.query.pages.firstChild;
+			var pageid = jsonObject.query.pageids[0];console.log(pageid);
+			var article = jsonObject.query.pages[pageid];
 			article.url = "http://en.wikipedia.org/wiki/" + encodeURIComponent(article.title);
 			article.link = "<a href='" + article.url + "'>" + article.title + "</a>";
-			article.text = cleanup( article.revisions[0].firstChild );
+			article.text = cleanup( article.revisions[0]["*"] );
 			$("#content").html("<h2>" + article.link + "</h2>");
 			$("#content").append( article.text );
 		}
