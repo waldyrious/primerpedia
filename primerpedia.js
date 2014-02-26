fc779275fe503f290749ef2589cff59f416196af
Waldir Pimenta <waldir@email.com>
2014-02-26 22:13:58 +0000

    activate search function (part 3 of 3: js)

--- primerpedia.js
+++ primerpedia.js
@@ -1,8 +1,7 @@
 var apiUrl = "http://en.wikipedia.org/w/api.php?";
 
-function onFormSubmit(){
-	// Loading animation from http://www.ajaxload.info/
-	$("#content").html("<img src='loading.gif' alt='Loading...' style='margin:1em 50%'>");
+function randomArticle(){
+	startSpinner();
 	// API request to load non-random page:
 	// action=parse&page=Concise_Wikipedia&section=0&prop=text&format=txtfm&disablepp
 	// first section of a random article. API query devised by http://stackoverflow.com/q/13517901/266309
@@ -24,4 +23,50 @@ function onFormSubmit(){
 			$("#content").append( article.extract );
 		}
 	});
+}
+
+function search(){
+	startSpinner();
+	searchTerm = $('#search-term')[0].value;
+	$.ajax({
+		url: apiUrl + "action=query&list=search&srsearch=" + searchTerm + "&srnamespace=0&srlimit=1&redirects=&format=json",
+		data: {
+		    format: "json"
+		},
+		dataType: "jsonp",
+		success: function(jsonObject){
+			loadFromSearch(jsonObject);
+		}
+	});
+}
+
+function loadFromSearch(searchResult){
+	var title = searchResult.query.search[0].title;
+	$.ajax({
+		// Option 1: Using action=parse
+		//url: apiUrl + "action=parse&page=" + title + "&section=0&prop=text&format=txtfm&disablepp",
+		// Option 2: Using action=query&prop=extracts
+		url: apiUrl + "action=query&prop=extracts&exintro&titles=" + title + "&indexpageids=true&format=json",
+		data: {
+		    format: "json"
+		},
+		dataType: "jsonp",
+		success: function(jsonObject){
+			// Option 1: if using action=parse
+			//$("#content").html(jsonObject.parse.text['*']);
+			// Option 2: if using action=query&prop=extracts
+			var pageid = jsonObject.query.pageids[0];
+			var article = jsonObject.query.pages[pageid];
+			article.url = "http://en.wikipedia.org/wiki/" + encodeURIComponent(article.title);
+			article.link = "<a href='" + article.url + "'>" + article.title + "</a>";
+			var editlink = "<a href='" + article.url + "?action=edit&section=0' class='edit-link'>edit</a>";
+			$("#content").html("<h2>" + article.link + editlink + "</h2>");
+			$("#content").append( article.extract );
+		}
+	});
+}
+
+function startSpinner(){
+	// Loading animation from http://www.ajaxload.info/
+	$("#content").html("<img src='loading.gif' alt='Loading...' style='margin:1em 50%'>");
 }
