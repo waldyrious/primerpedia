ab0e4270a816718eb4ae1f2b31af77ff3b60b7f0
Waldir Pimenta <waldir@email.com>
2014-02-27 21:31:01 +0000

    major refactoring, add more robustness, doc
    - use a single API query for search too
    - DRY: displayArticle() + startSpinner() = apiRequest()
    - consolidate scattered comments into a single descriptive text
      at the top of file, for clearer reading and future reference
    - make search more robust in case the term isn't found

--- primerpedia.js
+++ primerpedia.js
@@ -1,70 +1,62 @@
-var apiUrl = "http://en.wikipedia.org/w/api.php?";
+/* There are 3 ways (that I know about) of getting the first section of an article.
+ * 1) We can use action=parse coupled with the section=0 parameter,
+ *    but the "page" parameter needs to be supplied,
+ *    which means that we'd have to make two API requests
+ *    (either to get a random page, or to resolve a search term)
+ *    btw, this action seems to have a mobileformat= parameter, but the resuts are poor.
+ * 2) Another option is to use the prop=revisions query, coupled with rvprop=content,
+ *    &rvsection=0 and rvparse=true (see http://stackoverflow.com/q/13517901/266309).
+ *    This request can be combined with a random article generator,
+ *    using generator=random&grnnamespace=0,
+ *    or with a search generator, using generator=search.
+ *    But the parser returns the full html, so we'd have to clean it up ourselves.
+ * 3) Finally, we can use the prop=extracts query (from the Extension:MobileFrontend's extracts,
+ *    see https://www.mediawiki.org/wiki/Extension:MobileFrontend#prop.3Dextracts)
+ *    coupled with the "exintro" parameter. This can be paired
+ *    with a random page generator or a search generator.
+ *    The cleanup is pretty good, but links are removed (maybe not a bad thing)
+ *    and some templates are still displayed, so they need to be hidden via css
+ */
 
-function randomArticle(){
-	startSpinner();
-	// API request to load non-random page:
-	// action=parse&page=Concise_Wikipedia&section=0&prop=text&format=txtfm&disablepp
-	// first section of a random article. API query devised by http://stackoverflow.com/q/13517901/266309
-	// action=query&prop=revisions&rvprop=content&rvparse=&rvsection=0&generator=random&grnnamespace=0&indexpageids=true&format=json
-	$.ajax({
-		// https://www.mediawiki.org/wiki/Extension:MobileFrontend#prop.3Dextracts
-		url: apiUrl + "action=query&prop=extracts&exintro&generator=random&grnnamespace=0&indexpageids=true&format=json",
-		data: {
-		    format: "json"
-		},
-		dataType: "jsonp",
-		success: function(jsonObject) {
-			displayArticle(jsonObject);
-		}
-	});
+var apiUrl = "http://en.wikipedia.org/w/api.php?";
+// https://www.mediawiki.org/wiki/Extension:MobileFrontend#prop.3Dextracts
+var apiExtractsQuery = "action=query&prop=extracts&exintro&indexpageids=true&format=json";
+
+function random(){
+	apiRequest( apiExtractsQuery + "&generator=random&grnnamespace=0" );
 }
 
 function search(){
-	startSpinner();
 	searchTerm = $('#search-term')[0].value;
-	$.ajax({
-		url: apiUrl + "action=query&list=search&srsearch=" + searchTerm + "&srnamespace=0&srlimit=1&redirects=&format=json",
-		data: {
-		    format: "json"
-		},
-		dataType: "jsonp",
-		success: function(jsonObject){
-			loadFromSearch(jsonObject);
-		}
-	});
+	if(searchTerm)
+		apiRequest( apiExtractsQuery + "&generator=search&gsrlimit=1&gsrsearch=" + searchTerm );
+	else random();
 }
 
-function loadFromSearch(searchResult){
-	var title = searchResult.query.search[0].title;
-	$.ajax({
-		// Option 1: Using action=parse
-		//url: apiUrl + "action=parse&page=" + title + "&section=0&prop=text&format=txtfm&disablepp",
-		// Option 2: Using action=query&prop=extracts
-		url: apiUrl + "action=query&prop=extracts&exintro&titles=" + title + "&indexpageids=true&format=json",
-		data: {
-		    format: "json"
-		},
-		dataType: "jsonp",
-		success: function(jsonObject){
-			// Option 1: if using action=parse
-			//$("#content").html(jsonObject.parse.text['*']);
-			// Option 2: if using action=query&prop=extracts
-			displayArticle(jsonObject);
-		}
-	});
-}
-
-function displayArticle(articleData){
-	var pageid = articleData.query.pageids[0];
-	var article = articleData.query.pages[pageid];
-	article.url = "http://en.wikipedia.org/wiki/" + encodeURIComponent(article.title);
-	article.link = "<a href='" + article.url + "'>" + article.title + "</a>";
-	var editlink = "<a href='" + article.url + "?action=edit&section=0' class='edit-link'>improve this!</a>";
-	$("#content").html("<h2>" + article.link + editlink + "</h2>");
-	$("#content").append( article.extract );
-}
-
-function startSpinner(){
+function apiRequest(queryString){
 	// Loading animation from http://www.ajaxload.info/
 	$("#content").html("<img src='loading.gif' alt='Loading...' style='margin:1em 50%'>");
+	$.ajax({
+		url: apiUrl + queryString,
+		data: {
+		    format: "json"
+		},
+		dataType: "jsonp",
+		success: function(jsonObject){
+			var searchData = jsonObject.query.searchinfo;
+			if( typeof searchData === 'undefined' || searchData.totalhits > 0 ){ //
+				var pageid = jsonObject.query.pageids[0];
+				var article = jsonObject.query.pages[pageid];
+				article.url = "http://en.wikipedia.org/wiki/" + encodeURIComponent(article.title);
+				article.link = "<a href='" + article.url + "'>" + article.title + "</a>";
+				var editlink = "<a href='" + article.url + "?action=edit&section=0' class='edit-link'>improve this!</a>";
+				$("#content").html("<h2>" + article.link + editlink + "</h2>");
+				$("#content").append( article.extract );
+			} else if( typeof searchData.suggestion !== 'undefined' ){
+				apiRequest( apiExtractsQuery + "&generator=search&gsrlimit=1&gsrsearch=" + searchData.suggestion );
+			} else {
+				$("#content").html("<div class='error'>The search term wasn't found.</div>");
+			}
+		}
+	});
 }
